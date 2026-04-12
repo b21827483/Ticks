baebc5d3af46b7f4ba4f485f583be61936be2627
unknown <b21827483@cs.hacettepe.edu.tr>
2026-04-12 20:51:09 +0300

    jwt token provider created

---
+++ user_service/src/main/java/com/ticks/user_service/security/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.ticks.user_service.security;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.access-token-expiration-ms}")
+    private long accessTokenExpirationMs;
+
+    @Value("{$app.jwt.refresh-token-expiration-ms}")
+    private long refreshTokenExpirationMs;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateAccessToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()));
+        claims.put("type", "access");
+        return buildToken(claims, userDetails.getUsername(), accessTokenExpirationMs);
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("type", "refresh");
+        claims.put("jti", UUID.randomUUID().toString());
+        return buildToken(claims, userDetails.getUsername(), refreshTokenExpirationMs);
+    }
+
+    public String buildToken(Map<String, Object> claims, String subject, long expiration) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+
+    }
+}

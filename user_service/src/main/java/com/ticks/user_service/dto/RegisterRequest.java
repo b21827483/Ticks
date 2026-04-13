de4f5c93c13c03015c8f475819cdd5814ebec4e3
unknown <b21827483@cs.hacettepe.edu.tr>
2026-04-13 12:53:01 +0300

    register and login request dtos created

---
+++ user_service/src/main/java/com/ticks/user_service/dto/RegisterRequest.java
@@ -0,0 +1,34 @@
+package com.ticks.user_service.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 8 to 50 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers and underscores")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must contain at least one uppercase, lowercase, number, and special character")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50)
+    private String lastName;
+
+    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Invalid phone number")
+    private String phoneNumber;
+}
